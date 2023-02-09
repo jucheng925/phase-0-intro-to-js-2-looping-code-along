3b2ebccbc57ec34612b100c24d5a58c5c88d4b40
Julie C <jucheng925@gmail.com>
2023-02-09 10:00:58 -0500

    looping code-along

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const name = ["Annie", "Patrick", "Carmen"];
+let newArray = []
+
+function writeCards(name) {
+    for (let i = 0; i < name.length; i++) {
+        newArray[i] = `Thank you, ${name[i]}, for the wonderful surprise gift!`;
+    }
+    //console.log(newArray);
+    return newArray;
+   
+}
+writeCards(name);
+
+let integer = 10
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer)
+        integer--
+    }
+}
+countDown(integer)
